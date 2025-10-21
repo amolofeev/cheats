a6605ee78e60e021ff3a748360d70c4d9202f881
sane4eg <alexandr.nino@gmail.com>
2025-10-22 01:40:48 +0300

    update audit log

--- sql/change_log.sql
+++ sql/change_log.sql
@@ -1,24 +1,27 @@
-create table change_log(
-    schema text not null,
-    table_name text not null ,
-    operation text not null ,
-    pk jsonb not null,
-    changes jsonb not null,
-    created_at timestamptz not null default current_timestamp
-    -- тут не просто тк в триггер нельзя передать параметры из вне только через set local name = value
-    -- action text, ? типа в каком бизнес процессе это случилось (api/worker/руками залезли)
-    -- created_by text not null,
-    -- transaction_id uuid, ?
-    );
+drop table if exists event_log;
+create table event_log
+(
+    schema      text        not null,
+    table_name  text        not null,
+    operation   text        not null,
+    pk          jsonb       not null,
+    changes     jsonb       not null,
+    created_at  timestamptz not null default current_timestamp,
+    client_ip   text        not null,
+    client_port int         not null,
+    txid bigint not null,
+    client text not null
+);
 
-CREATE OR REPLACE FUNCTION change_log()
-RETURNS TRIGGER AS $$
-    DECLARE
-        diff jsonb := '{}';
-        pk_columns text[];
-        pk_values jsonb := '{}';
-        col_record text;
-        col_value text;
+CREATE OR REPLACE FUNCTION debug_function()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    diff       jsonb := '{}';
+    pk_columns text[];
+    pk_values  jsonb := '{}';
+    col_record text;
+    col_value  text;
 BEGIN
     IF TG_OP = 'INSERT' THEN
         diff = row_to_json(NEW);
@@ -28,9 +31,7 @@ BEGIN
         SELECT jsonb_object_agg(key, value)
         INTO diff
         FROM jsonb_each(to_jsonb(NEW)) AS new_data(key, value)
-        WHERE new_data.value IS DISTINCT FROM (
-            SELECT jsonb_extract_path(to_jsonb(OLD), new_data.key)
-        );
+        WHERE new_data.value IS DISTINCT FROM (SELECT jsonb_extract_path(to_jsonb(OLD), new_data.key));
     END IF;
     IF TG_OP = 'DELETE' THEN
         diff = row_to_json(OLD);
@@ -38,30 +39,45 @@ BEGIN
 
     IF diff is not null then
         -- некоторая магия, чтобы получить PK вида {col: val, col:val}
-        SELECT array_agg(a.attname) INTO pk_columns
+        SELECT array_agg(a.attname)
+        INTO pk_columns
         FROM pg_constraint c
-        JOIN pg_attribute a ON a.attrelid = c.conrelid AND a.attnum = ANY(c.conkey)
+                 JOIN pg_attribute a ON a.attrelid = c.conrelid AND a.attnum = ANY (c.conkey)
         WHERE c.conrelid = TG_RELID
-        AND c.contype = 'p';
+          AND c.contype = 'p';
         FOREACH col_record IN ARRAY pk_columns
-        LOOP
-            EXECUTE format('SELECT ($1).%I::text', col_record)
-            INTO col_value
-            USING CASE WHEN TG_OP in ('DELETE', 'UPDATE') THEN OLD ELSE NEW END;
-            pk_values = jsonb_set(pk_values, ARRAY[col_record], to_jsonb(col_value));
-        END LOOP;
+            LOOP
+                EXECUTE format('SELECT ($1).%I::text', col_record)
+                    INTO col_value
+                    USING CASE WHEN TG_OP in ('DELETE', 'UPDATE') THEN OLD ELSE NEW END;
+                pk_values = jsonb_set(pk_values, ARRAY [col_record], to_jsonb(col_value));
+            END LOOP;
         -- пишем дифф в БД.
-        -- TODO: добавить created_by и наверно transaction_id чтобы группировать изменения
-        insert into change_log(schema, table_name, operation, pk, changes)
-        values (TG_TABLE_SCHEMA, TG_TABLE_NAME, TG_OP, pk_values, diff);
+        insert into event_log(schema, table_name, operation, pk, changes, client_ip, client_port, txid, client)
+        values (
+                TG_TABLE_SCHEMA,
+                TG_TABLE_NAME,
+                TG_OP,
+                pk_values,
+                diff,
+                inet_client_addr()::text,
+                inet_client_port(),
+                txid_current(),
+                coalesce(current_setting('my_app.user_id', true), coalesce(current_setting('application_name', true), 'unknown')));
     end if;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 
--- триггер надо вешать на каждую конкретную таблицу в тч всякие m2m для полноты картины.
-CREATE TRIGGER change_log_on_<table_name>
-AFTER INSERT OR UPDATE OR DELETE ON <table_name>
-FOR EACH ROW
-EXECUTE FUNCTION change_log();
+-- триггер надо вешать на каждую конкретную таблицу в тч всякие m2m для полноты картины, а так же на саму таблицу аудит лога.
+CREATE TRIGGER test_event_log_on_users
+    AFTER INSERT OR UPDATE OR DELETE
+    ON users
+    FOR EACH ROW
+EXECUTE FUNCTION debug_function();
+
+/*
+set my_app.user_id = "xyz
+insert/update/delete
+*/
